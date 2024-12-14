a240376a50209cf1d2e71b77b09082fbbff16ce2
Deepika <deepzsree22@gmail.com>
2024-12-14 18:29:30 +0530

    i have committed

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@ const connectDB=async()=>{
 try{
   await mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
 console.log("mongoDB connected");
+console.log("njf");
 
 }catch(error){
     console.log('error while connecting mongodb',error.message);
